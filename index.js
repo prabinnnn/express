e7cab03d91f43cd671357dee8fa08be599d70e97
prabinnnn <prabinbhandari120@gmail.com>
2024-01-18 17:59:48 +0545

    add index

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const app = express();
 app.get("/", (req, res) => {
   res.json({ msg: "hello world" });
 });
+app.get("/prabin", (req, res) => {
+  res.json({ msg: "hello prabin" });
+});
 app.listen(8000, () => {
   console.log("application is running");
 });
