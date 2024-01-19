bb4890f7725e3c525b56e3f2aea3135c7a6aa584
prabinnnn <prabinbhandari120@gmail.com>
2024-01-19 16:27:46 +0545

    add index2

--- index2.js
+++ index2.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const app = express();
-app.get("/", (res, req) => {
-  res.json({ msg: "idcscdsnjcndsjcnsdcsdn" });
+
+app.get("/", (req, res) => {
+  let a = 2;
+  let b = 3;
+
+  let sum = a + b;
+
+  res.json({ msg: "14dgf45w12dfhgj", sum: sum });
 });
-app.listen(7777, () => {
-  console.log("app is running");
+
+app.listen(4000, () => {
+  console.log("App is running on port 4000");
 });
