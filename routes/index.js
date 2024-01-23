9a76e888bc00a572b014489bc1757d0ba3303657
prabinnnn <prabinbhandari120@gmail.com>
2024-01-23 20:59:13 +0545

    add index

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,19 @@
-const Router = require("express").Router();
+const router = require("express").Router();
 const bookrouter = require("./book.route");
 const blogrouter = require("./blog.route");
 const categoriesrouter = require("./categories.route");
 const rolesrouter = require("./roles.route");
-Router.get("/", (req, res) => {
+router.get("/", (req, res) => {
   res.json({ msg: "hello" });
 });
-Router.get("/:num1/:num2", (req, res) => {
+router.get("/:num1/:num2", (req, res) => {
   const { num1, num2 } = req.params;
   const sum = Number(num1) + Number(num2);
   res.json({ msg: `the ${sum} is ....` });
 });
-Router.use("/book", bookrouter);
-Router.use("/blog", blogrouter);
-Router.use("/categories", categoriesrouter);
-Router.use("/roles", rolesrouter);
+router.use("/book", bookrouter);
+router.use("/blog", blogrouter);
+router.use("/categories", categoriesrouter);
+router.use("/roles", rolesrouter);
 
-module.exports = Router;
+module.exports = router;
